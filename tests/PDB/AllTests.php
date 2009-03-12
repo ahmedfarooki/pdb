a40dc06809bed8fed982ca0172eb6417305e1638
Ian Eure <ian.eure@gmail.com>
2009-03-12 11:37:36 -0700

    Moved `AllTests.php` to `PDB/AllTests.php`, otherwise we break the class->path association

--- tests/PDB/AllTests.php
+++ tests/PDB/AllTests.php
@@ -3,29 +3,27 @@
 $path = '@test_dir@/@package-name@/tests';
 if (substr($path, 0, 1) == '@') {
     $path  = realpath(dirname(__FILE__));
-    $paths = array(
-        $path, realpath(dirname(__FILE__)) . '/../'
-    );
+    $paths = array($path, realpath(dirname(__FILE__) . '/../'));
 } else {
     $paths = array($path);
-}
-
-if (!file_exists($path . '/tests-config.php')) {
-    die("Unable to find tests-config.php file in $path!\n");    
 }
 
 set_include_path(implode(':', $paths) . ':' . get_include_path());
 
 require_once 'tests-config.php';
 require_once 'PHPUnit/Framework/TestSuite.php';
+require_once 'PDBTest.php';
 require_once 'PDB/mysqlTest.php';
+require_once 'PDB/sqliteTest.php';
 
-class AllTests
+class PDB_AllTests
 {
     public static function suite()
     {
         $suite = new PHPUnit_Framework_TestSuite('PHPUnit');
+        $suite->addTestSuite('PDBTest');
         $suite->addTestSuite('PDB_mysqlTest');
+        $suite->addTestSuite('PDB_sqliteTest');
         return $suite;
     }
 }
